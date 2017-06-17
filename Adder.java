9dff80c2ef4a3c72c4565a82c60e9e85f0825e3c
John Sawicki <jpsawicki@gmail.com>
2017-06-17 13:42:24 -0500

    Created Adder class

---
+++ Adder.java
@@ -0,0 +1,13 @@
+package app.cominjohnsawickitrkhp_identity_photo.linkedin.httpswww.gitpractice;
+
+/**
+ * Created by John on 6/17/2017.
+ */
+
+public class Adder {
+    public Adder(){
+    }
+    public int adding(int a, int b){
+        return a+b;
+    }
+}
---
+++ MainActivity.java
@@ -0,0 +1,17 @@
+package app.cominjohnsawickitrkhp_identity_photo.linkedin.httpswww.gitpractice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    int sum;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Adder a = new Adder();
+        sum = a.adding(3,5);
+        Toast.makeText(this, String.valueOf(sum), Toast.LENGTH_SHORT).show();
+    }
+}
